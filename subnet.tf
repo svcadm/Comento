1baca79f923a276addc94e859011eb716c1dd5ce
Hyunseokjung <hyunsukgo@naver.com>
2022-08-03 23:14:08 +0900

    Code 수정

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "comento-public-a" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.10.0.0/27"
-
+  availability_zone  = "ap-northeast-2a"
   tags = {
     Name = "Public-ELB-A"
   }
@@ -10,7 +10,7 @@ resource "aws_subnet" "comento-public-a" {
 resource "aws_subnet" "comento-public-c" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.10.0.32/27"
-
+  availability_zone  = "ap-northeast-2c"
   tags = {
     Name = "Public-ELB-C"
   }
@@ -19,7 +19,7 @@ resource "aws_subnet" "comento-public-c" {
 resource "aws_subnet" "comento-private-a" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.10.0.64/27"
-
+  availability_zone  = "ap-northeast-2a"
   tags = {
     Name = "Private-A"
   }
@@ -28,7 +28,7 @@ resource "aws_subnet" "comento-private-a" {
 resource "aws_subnet" "comento-private-c" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.10.0.96/27"
-
+  availability_zone  = "ap-northeast-2c"
   tags = {
     Name = "Private-C"
   }
